2bc983fdd9e6d4fae4e804f740c43b3fddfc408a
serhattatar <mserhattatar@gmail.com>
2021-01-31 13:35:59 +0300

    tictactoe project

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+
+    private GameController gameController;
+
+    public void SetSpace()
+    {
+        if(gameController.playerMove == true)
+        {
+            buttonText.text = gameController.GetPlayerSide();
+            button.interactable = false;
+            gameController.EndTrun();
+        }       
+    }
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+}
